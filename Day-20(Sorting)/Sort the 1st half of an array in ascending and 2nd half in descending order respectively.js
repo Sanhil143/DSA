0b1fd3b3ddf1c7643e8bbca71034b6935bc760cd
Sanhil143 <sanhilrai143@gmail.com>
2023-03-16 02:12:11 +0530

    Done

--- Day-20(Sorting)/Sort the 1st half of an array in ascending and 2nd half in descending order respectively.js
+++ Day-20(Sorting)/Sort the 1st half of an array in ascending and 2nd half in descending order respectively.js
@@ -20,4 +20,28 @@
 // Explanation: As we can see the array is sorted in ascending order till halfway, here till index 3 
 // (till element 4). After that from index 4(from element 9), the array is in descending order.
 
+ let arr = [ 5 ,2 ,4 ,7 ,9 ,3 ,1 ,6 ,8] // 1 2 3 4 9 8 7 6 5
  
+ const rotate= (arr) => { // nlogn o(1)
+      let n = arr.length
+      let temp = new Array(n)
+      let k = 0
+      
+     arr.sort((a,b) => a-b)
+
+     for(let i=0; i<Math.floor(arr.length/2); i++){
+      // temp.push(arr[i])
+      temp[k] = arr[i]
+      k++
+     }
+     for(let j=arr.length-1; j>=Math.floor(arr.length/2); j--){
+      // temp.push(arr[j])
+      temp[k] = arr[j]
+      k++
+     }
+     for(let k=0,i=0; k<temp.length; k++,i++){
+      arr[i] = temp[k]
+     }
+     return arr
+ }
+ console.log(rotate(arr));
