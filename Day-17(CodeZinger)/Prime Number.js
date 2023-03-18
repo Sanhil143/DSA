abfefb5dd877670abfadab14ef3fb9baf5a4f357
Sanhil143 <sanhilrai143@gmail.com>
2023-03-18 23:13:32 +0530

    Done

--- Day-17(CodeZinger)/Prime Number.js
+++ Day-17(CodeZinger)/Prime Number.js
@@ -10,9 +10,20 @@
 
 
 
-let n = 131077
+let n = 10
 
 const primeNum = (n) => {
-      
+      let prime = new Array(n);
+      let count = 0;
+      for (let i = 2; i < n; i++) {
+            if (prime[i]) continue;
+            count++;
+
+            for (let j = i; j * i < n; j++) {
+                  prime[j * i] = 1;
+            }
+      }
+      console.log(prime);
+      return count;
 }
 console.log((primeNum(n)));
