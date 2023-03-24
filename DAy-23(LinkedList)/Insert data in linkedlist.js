2a72a2b6dc7c69823b24dca9e55d23dd44151ccc
Sanhil143 <sanhilrai143@gmail.com>
2023-03-25 02:06:24 +0530

    Done

--- DAy-23(LinkedList)/Insert data in linkedlist.js
+++ DAy-23(LinkedList)/Insert data in linkedlist.js
@@ -1,4 +1,4 @@
-
+// inserting data on linked list
 
 
 
@@ -24,6 +24,7 @@ class LinkedList {
             return this.size;
       }
 
+      //Add element in start of node
       addFirst(value){
             const node = new Node(value)
             if(this.isEmpty()){
@@ -34,6 +35,7 @@ class LinkedList {
             }
             this.size++
       }
+      //Add element in end of node
       addLast(value){
             const node = new Node(value)
             if(this.isEmpty()){
@@ -45,14 +47,35 @@ class LinkedList {
                   }
                   lastNode.next = node;
             }
+            this.size++;
+      }
+      //Add element on the basis of index
+      addMid(value,index){
+            if(index < 0 || index > this.size){
+                  return;
+            }
+            if(index === 0){
+                  this.addFirst(value);
+            }
+            const node = new Node(value);
+            let prev = this.head;
+            for(let i=0; i<index-1; i++){
+                  prev = prev.next;
+            }
+            node.next = prev.next;
+            prev.next = node;
             this.size++
       }
+
 
 }
 
 let ll = new LinkedList()
 ll.addFirst(7)
 ll.addLast(27)
+ll.addLast(77)
+ll.addMid(17,1)
+ll.addMid(27,2)
 console.log(ll);
 // console.log(ll.isEmpty());
 // console.log(ll.getSize());
