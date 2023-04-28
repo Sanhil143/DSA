6a6802a61a27df26a20d7c95eb816394338d3f93
Sanhil143 <sanhilrai143@gmail.com>
2023-04-28 23:08:18 +0530

    Done

---
+++ Day-10(Recursion)/fibonacci num.js
@@ -0,0 +1,20 @@
+
+
+// nth fibonacci number
+
+
+let n = 10;
+
+const func = (n) => {
+      if(n === 0){
+            return 0;
+      }
+      else if(n === 1 || n === 2){
+            return 1;
+      }
+      else{
+            return func((n-1)+(n-2))
+      }
+}
+
+console.log(func(n));
