c3e4a25fa8d224bc5a0fa139eba0ddc0a3874f7f
Sanhil143 <sanhilrai143@gmail.com>
2023-05-23 23:13:51 +0530

    Done

---
+++ Day-40(Interview-task)/test.js
@@ -0,0 +1,41 @@
+// function getAge() {
+//       'use strict';
+//       age = 21;
+//       console.log(age);
+//     }
+    
+//     getAge();  21
+
+
+//     for (let i = 1; i < 5; i++) {
+//       if (i === 3) continue;
+//       console.log(i);
+//     }
+//     let number = 0;
+// console.log(number++);//0
+// console.log(++number); //2
+// console.log(number); //2
+
+// console.log(typeof typeof 1); //string
+
+
+// Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number)
+//  which has the maximum sum and return its sum.
+// let num = [1,2,3,-2,5]
+
+// let num = [-1,-2,-3,-4]
+// const func = (num) => {
+//       let max = -Infinity;
+//       let temp = 0;
+//       for(let i=0; i<num.length; i++){
+//             temp += num[i]
+//             if(temp > max){
+//                   max = temp;
+//             }
+//       }
+     
+//       return max;
+// }
+
+// console.log(func(num));
+
