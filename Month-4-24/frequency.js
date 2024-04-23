70ff591b655520ffa74c14b79ea278bfb38a1aba
Sanhil143 <sanhilrai143@gmail.com>
2024-04-24 00:09:11 +0530

    bitcot technology question

--- Month-4-24/frequency.js
+++ Month-4-24/frequency.js
@@ -1,8 +1,21 @@
-const arr =  [1,1,2,2,3,4,5,6,6,7]
-
-// result = {1:2,2:2,6:2}
+const arr =  [1,1,2,2,3,4,5,6,6,7];
 
 const frequency = (arr) => {
+  let output = {};
+  for(let i = 0; i < arr.length; i++) {
+    let n = arr[i];
+    if(output[n]) {
+      output[n]++;
+    } else {
+      output[n] = 1;
+    }
+  }
+  for(const key in output){
+    if(output[key] == 1){
+      delete output[key]
+    }
+  }
+  return output;
+};
 
-}
 console.log(frequency(arr));
